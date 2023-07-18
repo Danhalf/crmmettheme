95731a3abd4a566f1f1021fa18696aa849b3b497
Danhalf <danhel@rocketmail.com>
2023-07-18 07:08:08 +0300

    remove: redundant ts models

--- src/app/modules/auth/AuthLayout.tsx
+++ src/app/modules/auth/AuthLayout.tsx
@@ -17,19 +17,13 @@ const AuthLayout = () => {
 
   return (
     <div className='d-flex flex-column flex-lg-row flex-column-fluid h-100'>
-      {/* begin::Body */}
       <div className='d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1'>
-        {/* begin::Form */}
         <div className='d-flex flex-center flex-column flex-lg-row-fluid'>
-          {/* begin::Wrapper */}
           <div className='w-lg-500px p-10'>
             <Outlet />
           </div>
-          {/* end::Wrapper */}
         </div>
-        {/* end::Form */}
       </div>
-      {/* end::Body */}
     </div>
   )
 }
