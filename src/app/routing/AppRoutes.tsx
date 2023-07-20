750ae8ee33cf1bd0821a574537e86a63ca471b49
Danhalf <danhel@rocketmail.com>
2023-07-20 11:06:33 +0300

    add: routes

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -21,22 +21,20 @@ const {PUBLIC_URL} = process.env
 
 const AppRoutes: FC = () => {
   const {currentUser} = useAuth()
-
-  const userId = localStorage.getItem('user')?.length
   return (
     <BrowserRouter basename={PUBLIC_URL}>
       <Routes>
         <Route element={<App />}>
           <Route path='error/*' element={<ErrorsPage />} />
           <Route path='logout' element={<Logout />} />
-          {currentUser || userId ? (
+          {currentUser ? (
             <>
               <Route path='/*' element={<PrivateRoutes />} />
               <Route index element={<Navigate to='/dashboard' />} />
             </>
           ) : (
             <>
-              <Route index element={<AuthPage />} />
+              <Route path='/' element={<AuthPage />} />
               <Route path='*' element={<Navigate to='/' />} />
             </>
           )}
--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -1,6 +1,9 @@
 import {Route, Routes, Navigate} from 'react-router-dom'
 import {MasterLayout} from '../../_metronic/layout/MasterLayout'
 import {DashboardWrapper} from '../pages/dashboard/DashboardWrapper'
+import {UsersWrapper} from '../pages/users/UsersWrapper'
+import {ReportsWrapper} from '../pages/reports/ReportsWrapper'
+import {BillingWrapper} from '../pages/billing/BillingWrapper'
 
 const PrivateRoutes = () => {
   return (
@@ -10,6 +13,9 @@ const PrivateRoutes = () => {
         <Route path='/*' element={<Navigate to='/dashboard' />} />
         {/* Pages */}
         <Route path='dashboard' element={<DashboardWrapper />} />
+        <Route path='users' element={<UsersWrapper />} />
+        <Route path='reports' element={<ReportsWrapper />} />
+        <Route path='billing' element={<BillingWrapper />} />
         <Route path='*' element={<Navigate to='/error/404' />} />
       </Route>
     </Routes>
