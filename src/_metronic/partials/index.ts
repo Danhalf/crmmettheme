aaf7b4bdd1e4bec948370024d9f401cc4c4ecfaa
Danhalf <danhel@rocketmail.com>
2023-07-25 20:26:20 +0300

    fix: button disable on loading

--- src/_metronic/partials/index.ts
+++ src/_metronic/partials/index.ts
@@ -1,14 +1,5 @@
-export * from './layout/quick-links/QuicLinks'
-export * from './layout/search/Search'
-export * from './layout/search/SearchInner'
-export * from './layout/SearchModal'
-export * from './layout/InboxCompose'
 export * from './layout/header-menus/HeaderUserMenu'
-export * from './layout/header-menus/HeaderNotificationsMenu'
-export * from './layout/activity-drawer/ActivityDrawer'
-export * from './layout/create-app/CreateApp'
 export * from './layout/theme-mode/ThemeModeProvider'
-export * from './layout/theme-mode/ThemeModeSwitcher'
 
 // dropdpwns
 export * from './content/dropdown/Dropdown1'
@@ -20,7 +11,3 @@ export * from './content/editor/EditorToolbar'
 export * from './content/code-highlight/CodeBlock'
 
 export * from './content/portal/Portal'
-
-// modals
-export * from './modals/create-app-stepper/CreateAppModal'
-export * from './modals/select-location/SelectLocationModal'
