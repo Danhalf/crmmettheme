d5c94418def33a785e45e72fed9fbc029d2a98a5
Danhalf <danhel@rocketmail.com>
2023-07-31 22:57:46 +0300

    fix: billing text

--- src/components/dashboard/Billing.tsx
+++ src/components/dashboard/Billing.tsx
@@ -2,7 +2,7 @@ export const Billing = () => {
     return (
         <>
             <div className='mb-10'>
-                <h1 className='mb-5'>Users</h1>
+                <h1 className='mb-5'>Billing</h1>
                 <div className='card'>
                     <div className='card-body'>
                         <div className='table-responsive'>We're still working on this page</div>
