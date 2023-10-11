37f3e7bbd81993f6c04e1504a9cc42a4770da72a
danhalf <danhel@rocketmail.com>
2023-10-11 17:05:21 +0300

    added: token

--- src/router/privateRouter.tsx
+++ src/router/privateRouter.tsx
@@ -1,11 +1,18 @@
 import { STORAGE_USER } from 'app-consts';
 import { Login } from 'components/Login';
 import { Dashboard } from 'components/dashboard/Dashboard';
-import { LoginResponse } from 'services/auth.service';
+import { useEffect } from 'react';
+import { LoginResponse, checkToken } from 'services/auth.service';
+import { getToken } from 'services/utils';
 
 export const PrivateRoute = () => {
     const userStorage = localStorage.getItem(STORAGE_USER);
     const { useruid }: LoginResponse = userStorage ? JSON.parse(userStorage) : {};
+    const token = getToken();
+    useEffect(() => {
+        // eslint-disable-next-line no-console
+        checkToken(token).then((r) => console.log(r));
+    }, [token]);
 
     return useruid ? <Dashboard /> : <Login />;
 };
